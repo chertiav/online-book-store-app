3415d2153d926a032a215a38061cd4d9cbefd6e8
Andrey Chertykovtsev <chertiav@gmail.com>
2024-10-08 13:30:01 +0200

    Refactor codebase for better exception handling and DI - introduced `DataProcessingException`. - replaced manual dependency injection with Lombok's `@RequiredArgsConstructor`. - simplified `Book` class by replacing `@Data` with `@Getter` and `@Setter`. - added an example `application.properties` for MySQL settings.

--- src/main/java/com/achdev/onlinebookstoreapp/model/Book.java
+++ src/main/java/com/achdev/onlinebookstoreapp/model/Book.java
@@ -7,15 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
 public class Book {
--- src/main/java/com/achdev/onlinebookstoreapp/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/achdev/onlinebookstoreapp/repository/impl/BookRepositoryImpl.java
@@ -1,23 +1,19 @@
 package com.achdev.onlinebookstoreapp.repository.impl;
 
+import com.achdev.onlinebookstoreapp.exception.DataProcessingException;
 import com.achdev.onlinebookstoreapp.model.Book;
 import com.achdev.onlinebookstoreapp.repository.BookRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
@@ -32,7 +28,7 @@ public class BookRepositoryImpl implements BookRepository {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't save book: " + book, e);
+            throw new DataProcessingException("Can't save book: " + book, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -44,10 +40,9 @@ public class BookRepositoryImpl implements BookRepository {
     @Override
     public List<Book> findAll() {
         try (Session session = sessionFactory.openSession()) {
-            Query<Book> bookQuery = session.createQuery("from Book", Book.class);
-            return bookQuery.getResultList();
+            return session.createQuery("from Book", Book.class).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't find all books", e);
+            throw new DataProcessingException("Can't find all books", e);
         }
     }
 }
