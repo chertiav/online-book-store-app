8d3d42528b8580c70cf037eb8d7f876d52b795de
Andrey Chertykovtsev <chertiav@gmail.com>
2024-10-08 12:16:11 +0200

    Initial commit

---
+++ src/main/java/com/achdev/onlinebookstoreapp/OnlineBookStoreAppApplication.java
@@ -0,0 +1,11 @@
+package com.achdev.onlinebookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreAppApplication.class, args);
+    }
+}
